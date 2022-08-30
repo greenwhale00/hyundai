8d2eed4a7da6f64997f8e6bb9218fcc28bfac96b
Your Name <goreaykay@gmail.com>
2022-08-30 09:38:23 +0900

    220830

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ $(function () {
 
     $('.top_close_btn').on('click', function () {
         $('.TopBanner').addClass('on');
-    })
+        $('.Section01').addClass('on');
+    });
 
     $('.main_slider').slick({
         arrows: false,
@@ -11,7 +12,8 @@ $(function () {
 
     $(window).on('scroll', function () {
         var sct = $(window).scrollTop();
-        sct > 0 ? $('.Header').addClass('on')
+        sct > 0
+            ? $('.Header').addClass('on')
             : $('.Header').removeClass('on')
     })
 
